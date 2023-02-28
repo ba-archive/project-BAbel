5f6f8309442d60794f293de33bf635b8ea647afa
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-02-28 23:54:58 +0900

    fix(anchor): 缓解 ssr 导致不能准确定位到锚点的问题

--- docs/utils/anchorPositioningService.ts
+++ docs/utils/anchorPositioningService.ts
@@ -1,15 +1,24 @@
-function scrollElementIntoView(element: HTMLElement) {
-  // const navBarHeight = parseInt(
-  //   getComputedStyle(document.body)
-  //     .getPropertyValue('--vp-nav-height')
-  //     .replace('px', '')
-  // );
-
-  const top = element.offsetTop;
-
-  window.scrollTo({
-    top: top,
-  });
+function isInViewport(el: HTMLElement) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
-export { scrollElementIntoView };
+function scrollElementIntoView(element: HTMLElement) {
+  setTimeout(() => {
+    if (!isInViewport(element)) {
+      const top = element.offsetTop;
+
+      window.scrollTo({
+        top: top,
+      });
+    }
+  }, 500);
+}
+
+export { scrollElementIntoView, isInViewport };
